9397cb52fed51760aed301b32fe6f3b7b2c8de2c
Kishan Kokal <kokalkishan.official@gmail.com>
2024-05-09 12:01:44 +0530

    :heavy_plus_sign: Add CORS middleware for cross-origin resource sharing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
 import express, { Express } from "express";
 import { PORT } from "./config.js";
 import { queryController } from "./controllers.js";
+import cors from "cors";
 
 const app: Express = express();
 app.use(express.json());
+app.use(cors());
 
 app.post("/api/query", queryController);
 
